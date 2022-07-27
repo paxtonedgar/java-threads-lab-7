502d496aa9e30068c77d57d0d70203f90a14e895
Paxton Edgar <paxtonedgar3@gmail.com>
2022-07-27 14:05:57 -0400

    init push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        // create an executor
+        Integer number = getCallableInteger().call();
+        System.out.println(number);
+    }
     public static Callable<Integer> getCallableInteger() {
-        // your code here
+    Callable<Integer> number = () -> {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        return (Integer)num;
+    };
+    return number;
     }
 }
